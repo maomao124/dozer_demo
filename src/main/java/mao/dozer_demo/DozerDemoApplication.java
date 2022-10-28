13440f53659280a9f4863d9a423fa58ed8084d91
mao <1296193245@qq.com>
2022-10-28 19:47:13 +0800

    dozer_demo

---
+++ src/main/java/mao/dozer_demo/DozerDemoApplication.java
@@ -0,0 +1,15 @@
+package mao.dozer_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DozerDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DozerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/dozer_demo/DozerDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.dozer_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DozerDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
